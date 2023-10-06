50367f33ea6831bc8e623b9d062fa57bd052621b
Marta Scardillo <martascardillo02@gmail.com>
2023-10-06 17:28:16 +0200

    Add some decorative stuff

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 let result;
+let className = "box p-2 square mx-2 d-inline-flex justify-content-center align-items-center ";
 
 for (let i = 0; i < 100; i++) {
   const n = i + 1;
@@ -7,21 +8,28 @@ for (let i = 0; i < 100; i++) {
   const buzz = n % 5;
 
   if (fizz === 0 && buzz === 0) {
-    console.log("BuzzFizz");
-    result = "BuzzFizz";
+    console.log("FizzBuzz");
+    result = "FizzBuzz";
+    className = className + "bg-danger";
   } else if (buzz === 0) {
     console.log("Buzz");
     result = "Buzz";
+    className = className + "bg-warning";
   } else if (fizz === 0) {
     console.log("Fizz");
     result = "Fizz";
+    className = className + "bg-success";
   } else {
     console.log(n);
     result = n;
+    className = className + "bg-primary";
   }
 
   let div = document.getElementById("container");
   let paragraph = document.createElement("p");
+  paragraph.classList = className;
   paragraph.innerHTML = result;
   div.append(paragraph);
+
+  className = "box p-2 square mx-2 d-inline-flex justify-content-center align-items-center ";
 }
